12acb2c72062c6cd025c91106ed229b3a5322ca0
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-09-01 05:06:20 +0900

    Attribute Set

--- Source/Project_V/Public/AbilitySystem/PlaygroundAttributeSet.h
+++ Source/Project_V/Public/AbilitySystem/PlaygroundAttributeSet.h
@@ -3,8 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystem/PlaygroundAbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "PlaygroundAttributeSet.generated.h"
+
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +21,32 @@ UCLASS()
 class PROJECT_V_API UPlaygroundAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	//속성 함수
+	UPlaygroundAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UPlaygroundAttributeSet, CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UPlaygroundAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UPlaygroundAttributeSet, CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UPlaygroundAttributeSet, MaxRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UPlaygroundAttributeSet, AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UPlaygroundAttributeSet, DefensePower)
 };
