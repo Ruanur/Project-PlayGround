0d03cdb8b1b2dd29a8064eb8e26ffa12fb532457
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-09-28 16:53:13 +0900

    Rotate Enemy In Task

--- Source/Project_V/Public/AI/BTTask_RotateToFaceTarget.h
+++ Source/Project_V/Public/AI/BTTask_RotateToFaceTarget.h
@@ -40,6 +40,11 @@ class PROJECT_V_API UBTTask_RotateToFaceTarget : public UBTTaskNode
 	virtual FString GetStaticDescription() const override;
 	// ~End UBTNode Interface
 
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	bool HasReachedAnglePercision(APawn* QueryPawn, AActor* TargetActor) const;
+
 	UPROPERTY(EditAnywhere, Category = "Face Target")
 	float AnglePrecision;
 
