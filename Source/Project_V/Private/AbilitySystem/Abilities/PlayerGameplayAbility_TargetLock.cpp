958dd6ff43c1b4113a11770edbca014441e56a98
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-10-04 01:44:27 +0900

    Get Available Targets

--- Source/Project_V/Private/AbilitySystem/Abilities/PlayerGameplayAbility_TargetLock.cpp
+++ Source/Project_V/Private/AbilitySystem/Abilities/PlayerGameplayAbility_TargetLock.cpp
@@ -2,13 +2,56 @@
 
 
 #include "AbilitySystem/Abilities/PlayerGameplayAbility_TargetLock.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Characters/PlaygroundPlayerCharacter.h"
+
+#include "PlaygroundDebugHelper.h"
 
 void UPlayerGameplayAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
+	TryLockOnTarget();
+
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 }
 
 void UPlayerGameplayAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UPlayerGameplayAbility_TargetLock::TryLockOnTarget()
+{
+	GetAvailableActorsToLock();
+}
+
+void UPlayerGameplayAbility_TargetLock::GetAvailableActorsToLock()
+{
+	TArray<FHitResult> BoxTraceHits;
+
+	UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetPlayerCharacterFromActorInfo(),
+		GetPlayerCharacterFromActorInfo()->GetActorLocation(),
+		GetPlayerCharacterFromActorInfo()->GetActorLocation() + GetPlayerCharacterFromActorInfo()->GetActorForwardVector() * BoxTraceDistance,
+		TraceBoxSize / 2.f,
+		GetPlayerCharacterFromActorInfo()->GetActorForwardVector().ToOrientationRotator(),
+		BoxTraceChannal,
+		false,
+		TArray<AActor*>(),
+		bShowPersistanceDebugShape ? EDrawDebugTrace::Persistent : EDrawDebugTrace::None,
+		BoxTraceHits,
+		true
+	);
+
+	for (const FHitResult& TraceHit : BoxTraceHits)
+	{
+		if (AActor* HitActor = TraceHit.GetActor())
+		{
+			if (HitActor != GetPlayerCharacterFromActorInfo())
+			{
+				AvailableActorsToLock.AddUnique(HitActor);
+
+				Debug::Print(HitActor->GetActorNameOrLabel());
+			}
+		}
+	}
 }
