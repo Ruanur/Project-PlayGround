4fb26ffaa8a8f5656c3e3eff7e9fbd88e3bd3031
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-10-06 05:32:57 +0900

    Tweaking Target Lock
    타겟 락 대상 사망 시 크래시 문제 해결
    타겟 락 스탠스 중 카메라 위치 수정

--- Source/Project_V/Private/AbilitySystem/Abilities/PlayerGameplayAbility_TargetLock.cpp
+++ Source/Project_V/Private/AbilitySystem/Abilities/PlayerGameplayAbility_TargetLock.cpp
@@ -44,6 +44,7 @@ void UPlayerGameplayAbility_TargetLock::OnTargetLockTick(float DeltaTime)
 		)
 	{
 		CancelTargetLockAbility();
+		return;
 	}
 
 	SetTargetLockWidgetPosition();
@@ -54,11 +55,11 @@ void UPlayerGameplayAbility_TargetLock::OnTargetLockTick(float DeltaTime)
 
 	if (bShouldOverrideRotation)
 	{
-		const FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(
+		FRotator LookAtRot = UKismetMathLibrary::FindLookAtRotation(
 			GetPlayerCharacterFromActorInfo()->GetActorLocation(),
 			CurrentLockedActor->GetActorLocation());
 
-
+		LookAtRot -= FRotator(TargetLoackCameraOffSetDistance, 0.f, 0.f);
 
 		const FRotator CurrentControlRot = GetPlayerControllerFromActorInfo()->GetControlRotation();
 		const FRotator TargetRot = FMath::RInterpTo(CurrentControlRot, LookAtRot, DeltaTime, TargetLockRotationInterpSpeed);
