0ad228e8fb77aa4533b4462d2ecdaf7f61cbf97b
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-10-07 07:28:06 +0900

    Projectile Class

--- Source/Project_V/Project_V.Build.cs
+++ Source/Project_V/Project_V.Build.cs
@@ -17,7 +17,8 @@ public class Project_V : ModuleRules
 			"GameplayTags",
 			"GameplayTasks",
             "AnimGraphRuntime",
-			"MotionWarping"});
+			"MotionWarping",
+			"Niagara"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ Source/Project_V/Public/Items/PlaygroundProjectileBase.h
@@ -0,0 +1,44 @@
+// Copyright 2025. Jinsol Co. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlaygroundProjectileBase.generated.h"
+
+class UBoxComponent;
+class UNiagaraComponent;
+class UProjectileMovementComponent;
+
+UENUM(BlueprintType)
+enum class EProjectileDamagePolicy : uint8
+{
+	OnHit,
+	OnBeginOverlap
+};
+
+UCLASS()
+class PROJECT_V_API APlaygroundProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlaygroundProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	UBoxComponent* ProjectileCollisionBox;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	UNiagaraComponent* ProjectileNiagaraComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	UProjectileMovementComponent* ProjectileMovementComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Projectile")
+	EProjectileDamagePolicy ProjectileDamagePolicy = EProjectileDamagePolicy::OnHit;
+};
