ba5ef34d14cfc674e22943c6de10cfb8254d487b
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-10-12 11:45:47 +0900

    Listen For Gameplay Event

--- Source/Project_V/Public/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h
+++ Source/Project_V/Public/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h
@@ -35,6 +35,11 @@ public:
 	UPROPERTY(BlueprintAssignable);
 	FWaitSpawnEnemiesDelegate DidNotSpawn;
 
+	// ~Begin UGameplayTask Interface
+	virtual void Activate() override;
+	virtual void OnDestroy(bool bInOwnerFinished) override; 
+	// ~End UGameplayTask Interface
+
 private:
 	FGameplayTag CachedEventTag;
 	TSoftClassPtr<APlaygroundEnemyCharacter> CachedSoftEnemyClassToSpawn;
@@ -42,4 +47,7 @@ private:
 	FVector CachedSpawnOrigin;
 	float CachedRandomSpawnRadius;
 	FRotator CachedSpawnRotation;
+	FDelegateHandle DelegateHandle;
+
+	void OnGameplayEventReceived(const FGameplayEventData* InPayload);
 };
