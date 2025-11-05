ee4fda8f20fa3b976586a71f8b60049ed2374c60
Minkj <91407973+Ruanur@users.noreply.github.com>
2025-11-06 04:10:39 +0900

    Main Hub Collision Set
    메인 허브 콜리젼 설정

--- Source/Project_V/Private/PlaygroundGameInstance.cpp
+++ Source/Project_V/Private/PlaygroundGameInstance.cpp
@@ -3,6 +3,10 @@
 
 #include "PlaygroundGameInstance.h"
 #include "MoviePlayer.h"
+#include "Blueprint/UserWidget.h"
+
+#include "PlaygroundDebugHelper.h"
+
 
 void UPlaygroundGameInstance::Init()
 {
@@ -10,22 +14,48 @@ void UPlaygroundGameInstance::Init()
 
 	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &ThisClass::OnPreLoadMap);
 	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnDestinationWorldLoaded);
+
+	Debug::Print(TEXT("Init Run"));
 }
 
 void UPlaygroundGameInstance::OnPreLoadMap(const FString& MapName)
 {
+	const auto LoadingWidget = CreateWidget<UUserWidget>(this, LoadingWidgetClass.LoadSynchronous(), TEXT("LoadingScreen"));
+	TSharedRef<SWidget> LoadingSWidgetPtr = LoadingWidget->TakeWidget();
+
 	FLoadingScreenAttributes LoadingScreenAttributes;
+
 	LoadingScreenAttributes.bAutoCompleteWhenLoadingCompletes = true;
+
+	//로딩 화면 최소 2초 유지
 	LoadingScreenAttributes.MinimumLoadingScreenDisplayTime = 2.f;
-	LoadingScreenAttributes.WidgetLoadingScreen = FLoadingScreenAttributes::NewTestLoadingScreenWidget();
+
+	//LoadingScreenAttributes.WidgetLoadingScreen = FLoadingScreenAttributes::NewTestLoadingScreenWidget();
+	LoadingScreenAttributes.WidgetLoadingScreen = LoadingSWidgetPtr;
 
 	GetMoviePlayer()->SetupLoadingScreen(LoadingScreenAttributes);
+
+	Debug::Print(TEXT("OnPreLoadMap Run"));
 }
 
 void UPlaygroundGameInstance::OnDestinationWorldLoaded(UWorld* LoadedWorld)
 {
+	//로딩 종료
 	GetMoviePlayer()->StopMovie();
+	Debug::Print(TEXT("OnDestinationWorldLoaded Run"));
 }
+
+TSoftObjectPtr<UWorld> UPlaygroundGameInstance::GetLevel(FString LevelName)
+{
+	if (MapList.IsEmpty())
+	{
+		Debug::Print(TEXT("Map List Is Null Please Fill"));
+		return nullptr;
+	}
+
+	return *MapList.Find(LevelName);
+}
+
 
 TSoftObjectPtr<UWorld> UPlaygroundGameInstance::GetGameLevelByTag(FGameplayTag InTag) const
 {
--- Source/Project_V/Public/PlaygroundGameInstance.h
+++ Source/Project_V/Public/PlaygroundGameInstance.h
@@ -35,14 +35,29 @@ class PROJECT_V_API UPlaygroundGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
-	virtual void Init() override;
+	TSoftObjectPtr<UWorld> GetLevel(FString LevelName);
 
 protected:
+	//게임 인스턴스가 처음 생성될 때 호출
+	virtual void Init() override;
+
+	//맵 로드가 시작되기 전에 로딩 화면 설정
+	UFUNCTION()
 	virtual void OnPreLoadMap(const FString& MapName);
+
+	//맵이 로드된 후 로딩 화면을 종료
+	UFUNCTION()
 	virtual void OnDestinationWorldLoaded(UWorld* LoadedWorld);
+
+	UPROPERTY(EditDefaultsOnly)
+	TSoftClassPtr<UUserWidget> LoadingWidgetClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TArray<FPlaygroundGameLevelSet> GameLevelSets;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Level")
+	TMap<FString, TSoftObjectPtr<UWorld>> MapList;
 
 public:
 	UFUNCTION(BlueprintPure, meta = (GameplayTagFliter = "GameData.Level"))
