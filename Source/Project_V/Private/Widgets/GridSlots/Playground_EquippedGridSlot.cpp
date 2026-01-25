b23795df716c317ded25235edae364e3cc267746
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-01-26 06:32:15 +0900

    Slotted Item Draw Size

--- Source/Project_V/Private/Widgets/GridSlots/Playground_EquippedGridSlot.cpp
+++ Source/Project_V/Private/Widgets/GridSlots/Playground_EquippedGridSlot.cpp
@@ -5,6 +5,9 @@
 #include "Widgets/HoverItem/Playground_HoverItem.h"
 #include "Inventory/Utils/Playground_InventoryStatics.h"
 #include "Components/Image.h"
+#include "Items/Drops/Playground_InventoryItem.h"
+#include "Items/Fragment/Playground_FragmentTags.h"
+#include "Items/Fragment/Playground_ItemFragment.h"
 
 void UPlayground_EquippedGridSlot::NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
 {
@@ -41,15 +44,34 @@ FReply UPlayground_EquippedGridSlot::NativeOnMouseButtonDown(const FGeometry& In
 UPlayground_EquippedSlottedItem* UPlayground_EquippedGridSlot::OnItemEquipped(UPlayground_InventoryItem* Item, const FGameplayTag& EquipmentTag, float TileSize)
 {
 	// Check the Equipment Type Tag
+	if (!EquipmentTag.MatchesTagExact(EquipmentTag)) return nullptr;
+	 
 	// Get Grid Dimensions
+	
+	
 	// Calculate the Draw Size for the Equipped Slotted Item
+	const FPlayground_GridFragment* GridFragment = GetFragment<FPlayground_GridFragment>(Item, FragmentTags::GridFragment);
+	if (!GridFragment) return nullptr;
+
+	const FIntPoint GridDimensions = GridFragment->GetGridSize();
+
+	const float IconTileWidth = TileSize - GridFragment->GetGridPadding() * 2;
+	const FVector2D DrawSize = GridDimensions * IconTileWidth;
+
 	// Create the Equipped Slotted Item widget
+	
 	// Set the Slotted Item's Inventory Item
+	
 	// Set the Slotted Item's Equipment Type Tag
+	
 	// Hide the Stack Count widget on the Slotted Item
+	
 	// Set Inventory Item on this class (The Equipped Grid Slot)
+	
 	// Set the Image Brush on the Equipped Slotted item
+	
 	// Add the Slotted Item as a child to this widget's Overlay
+	
 	// Return the Equipped Slotted item widget
 
 	return nullptr;
